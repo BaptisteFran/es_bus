1ce229a5823f692242b99b03241f65678e12230f
Kerstern <baptiste.franel@gmail.com>
2017-04-13 10:38:06 +0200

    Create bus_sv.lua

---
+++ bus_sv.lua
@@ -0,0 +1,12 @@
+
+RegisterServerEvent('bus:success')
+AddEventHandler('bus:success', function()
+  print("Player ID " ..source)
+	-- Get the players money amount
+TriggerEvent('es:getPlayerFromId', source, function(user)
+  total = math.random(100, 500);
+  -- update player money amount
+  user:addMoney((total + 0.0))
+ TriggerClientEvent("es_freeroam:notify", source, "CHAR_BANK_MAZE", 1, "Maze Bank", false, "Your money has been updated to ~g~$".. tonumber(total))
+ end)
+end)
