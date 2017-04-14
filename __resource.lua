81f7e39bd39d08183882cd6d4c82cd5732a3c1f2
Kerstern <baptiste.franel@gmail.com>
2017-04-14 18:25:32 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,2 +1,11 @@
-client_script 'bus.lua'
-server_script 'bus_sv.lua'
+resource_manifest_version "77731fab-63ca-442c-a67b-abc70f28dfa5"
+
+server_scripts {
+		"bus_sv.lua",
+} 
+
+client_scripts {
+		"bus.lua",
+}
+
+
