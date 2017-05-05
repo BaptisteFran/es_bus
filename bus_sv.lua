2a60251eace16e9ee474e26f26f0b2fd360274a2
Kerstern <baptiste.franel@gmail.com>
2017-05-05 10:35:21 +0200

    Update bus_sv.lua

--- bus_sv.lua
+++ bus_sv.lua
@@ -1,7 +1,19 @@
+require "resources/essentialmode/lib/MySQL"
+MySQL:open("localhost", "gta5_gamemode_essential", "root", "password")
+
+
+function nameJob(id)
+  local executed_query = MySQL:executeQuery("SELECT * FROM jobs WHERE job_id = '@namejob'", {['@namejob'] = id})
+  local result = MySQL:getResults(executed_query, {'job_name'}, "job_id")
+  return result[1].job_name
+end
+
 
 RegisterServerEvent('busJob:addMoney')
 AddEventHandler('busJob:addMoney', function(amount)
   TriggerEvent('es:getPlayerFromId', source, function(user)
+        local nameJob = nameJob(id)
+       if nameJob == 6 then -- Bus driver job ID  
     user:addMoney((amount))
      end)
 end)
