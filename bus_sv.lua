2499e2d3a50dd39b1a133449a19434b37e19ea7d
Kerstern <baptiste.franel@gmail.com>
2017-05-05 10:29:00 +0200

    Update bus_sv.lua

--- bus_sv.lua
+++ bus_sv.lua
@@ -1,1 +1,7 @@
 
+RegisterServerEvent('busJob:addMoney')
+AddEventHandler('busJob:addMoney', function(amount)
+  TriggerEvent('es:getPlayerFromId', source, function(user)
+    user:addMoney((amount))
+     end)
+end)
